a5a2572a0f79a281168c08c8c4d6fa27892e6c6f
Ricardo SIGNES <rjbs@cpan.org>
2009-01-31 22:07:20 -0500

    Sender:: not Send:: links

--- lib/Email/Sender.pm
+++ lib/Email/Sender.pm
@@ -16,8 +16,8 @@ which did a decent job at handling very simple email sending tasks, but was not
 suitable for serious use, for a variety of reasons.
 
 At present, the casual user is probably best off using
-L<Email::Send::Transport::Sendmail>.  If a local F<sendmail> program is
-unavailable, L<Email::Send::Transport::SMTP> will allow you to send mail
+L<Email::Sender::Transport::Sendmail>.  If a local F<sendmail> program is
+unavailable, L<Email::Sender::Transport::SMTP> will allow you to send mail
 through your relay host.
 
 In the future, L<Email::Sender::Simple> will provide a very simple interface

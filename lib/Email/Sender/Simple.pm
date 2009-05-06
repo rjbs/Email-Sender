fca3f547bddf6f4e2fa37308261b18004ad00a92
Ricardo SIGNES <rjbs@cpan.org>
2009-05-05 23:36:01 -0400

    first pass at converting ICG::Sendmail into Email::Sender::Simple

--- lib/Email/Sender/Simple.pm
+++ lib/Email/Sender/Simple.pm
@@ -1,15 +1,71 @@
 package Email::Sender::Simple;
-use strict;
-use warnings;
+use Moose;
 
-=head1 NAME
+use Sub::Exporter::Util ();
+use Sub::Exporter -setup => {
+  exports => { sendmail => Sub::Exporter::Util::curry_class('send') },
+};
 
-Email::Sender::Simple - send mail simply (not yet implemented)
+{
+  my $DEFAULT_TRANSPORT;
+  my $DEFAULT_FROM_ENV;
 
-=cut
+  sub _default_was_from_env {
+    my ($self) = @_;
+    $self->_default_transport;
+    return $DEFAULT_FROM_ENV;
+  }
 
-our $VERSION = '0.004';
+  sub _default_transport {
+    return $DEFAULT_TRANSPORT if $DEFAULT_TRANSPORT;
+    
+    if ($ENV{EMAIL_SENDER_TRANSPORT}) {
+      my $transport_class = $ENV{EMAIL_SENDER_TRANSPORT};
 
-# This is gonna be good. -- rjbs, 2008-12-10
+      if ($transport_class !~ tr/://) {
+        $transport_class = "Email::Send::Mailer::$transport_class";
+      }
 
-1;
+      eval "require $transport_class" or die $@;
+
+      my %arg;
+      for my $key (grep { /^EMAIL_SENDER_TRANSPORT_\w+/ } keys %ENV) {
+        (my $new_key = $key) =~ s/^EMAIL_SENDER_TRANSPORT_//;
+        $arg{$new_key} = $ENV{$key};
+      }
+
+      $DEFAULT_FROM_ENV  = 1;
+      $DEFAULT_TRANSPORT = $transport_class->new(\%arg);
+    } else {
+      require Email::Send::Mailer::SMTP;
+      $DEFAULT_FROM_ENV  = 0;
+      $DEFAULT_TRANSPORT = Email::Send::Mailer::SMTP->new;
+    }
+
+    return $DEFAULT_TRANSPORT;
+  }
+
+  sub reset_default_transport {
+    undef $DEFAULT_TRANSPORT;
+    undef $DEFAULT_FROM_ENV;
+  }
+}
+
+sub send {
+  my ($self, $message, $arg) = @_;
+
+  Carp::cluck "ICG::Sendmail->sendmail in non-void context considered harmful"
+    if defined wantarray;
+
+  my $transport = $self->_default_transport;
+
+  if ($arg->{transport}) {
+    $arg = { %$arg }; # So we can delete mailer without ill effects.
+    $transport = delete $arg->{transport} unless $self->_default_was_from_env;
+  }
+
+  $transport->send_email($message, $arg);
+}
+
+no Moose;
+"220 OK";
